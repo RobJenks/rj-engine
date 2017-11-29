8d7f45e5c1bd5a3829e99a3a5ea4ab22415406db
Rob Jenks <robjenks@gmail.com>
2017-11-29 23:00:01 +0000

    Implemented constant buffer object with binding and unbinding logic

---
+++ RJ/ConstantBufferDX11.cpp
@@ -0,0 +1,89 @@
+#include "ConstantBufferDX11.h"
+#include "DX11_Core.h"
+#include "CoreEngine.h"
+
+
+// Static null buffer resource, used for more efficient unbinding
+ID3D11Buffer * const ConstantBufferDX11::m_null_buffer[1] = { nullptr };
+
+
+// Constructor; create a new constant buffer of the given size
+ConstantBufferDX11::ConstantBufferDX11(UINT buffer_size)
+	:
+	m_buffersize(buffer_size)
+{
+	assert(buffer_size > 0);
+
+	D3D11_BUFFER_DESC bufferdesc;
+	memset(&bufferdesc, 0, sizeof(D3D11_BUFFER_DESC));
+
+	bufferdesc.Usage = D3D11_USAGE_DYNAMIC;
+	bufferdesc.ByteWidth = m_buffersize;
+	bufferdesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bufferdesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	bufferdesc.MiscFlags = 0;
+	bufferdesc.StructureByteStride = 0;
+
+	m_buffer[0] = NULL;
+	if (FAILED(Game::Engine->GetDevice()->CreateBuffer(&bufferdesc, NULL, &(m_buffer[0]))))
+	{
+		Game::Log << LOG_ERROR << "Failed to create constant buffer resource (size: " << m_buffersize << ")\n";
+	}
+}
+
+// Bind this resource to the given shader target
+void ConstantBufferDX11::Bind(Shader::Type shadertype, Shader::SlotID slot_id)
+{
+	switch (shadertype)
+	{
+		case Shader::Type::VertexShader:
+			Game::Engine->GetDeviceContext()->VSSetConstantBuffers(slot_id, 1, m_buffer);		
+			break;
+		case Shader::Type::PixelShader:
+			Game::Engine->GetDeviceContext()->PSSetConstantBuffers(slot_id, 1, m_buffer);
+			break;
+		case Shader::Type::HullShader:
+			Game::Engine->GetDeviceContext()->HSSetConstantBuffers(slot_id, 1, m_buffer);
+			break;
+		case Shader::Type::DomainShader:
+			Game::Engine->GetDeviceContext()->DSSetConstantBuffers(slot_id, 1, m_buffer);
+			break;
+		case Shader::Type::GeometryShader:
+			Game::Engine->GetDeviceContext()->GSSetConstantBuffers(slot_id, 1, m_buffer);
+			break;
+		case Shader::Type::ComputeShader:
+			Game::Engine->GetDeviceContext()->CSSetConstantBuffers(slot_id, 1, m_buffer);
+			break;
+	}
+}
+
+// Remove this (or any) binding from the given shader target
+void ConstantBufferDX11::Unbind(Shader::Type shadertype, Shader::SlotID slot_id)
+{
+	switch (shadertype)
+	{
+		case Shader::Type::VertexShader:
+			Game::Engine->GetDeviceContext()->VSSetConstantBuffers(slot_id, 1, m_null_buffer);
+			break;
+		case Shader::Type::PixelShader:
+			Game::Engine->GetDeviceContext()->PSSetConstantBuffers(slot_id, 1, m_null_buffer);
+			break;
+		case Shader::Type::HullShader:
+			Game::Engine->GetDeviceContext()->HSSetConstantBuffers(slot_id, 1, m_null_buffer);
+			break;
+		case Shader::Type::DomainShader:
+			Game::Engine->GetDeviceContext()->DSSetConstantBuffers(slot_id, 1, m_null_buffer);
+			break;
+		case Shader::Type::GeometryShader:
+			Game::Engine->GetDeviceContext()->GSSetConstantBuffers(slot_id, 1, m_null_buffer);
+			break;
+		case Shader::Type::ComputeShader:
+			Game::Engine->GetDeviceContext()->CSSetConstantBuffers(slot_id, 1, m_null_buffer);
+			break;
+	}
+}
+
+// Default destructor
+ConstantBufferDX11::~ConstantBufferDX11(void)
+{
+}
--- RJ/ConstantBufferDX11.h
+++ RJ/ConstantBufferDX11.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "DX11_Core.h"
 #include "Shader.h"
 
 
@@ -7,14 +8,25 @@ class ConstantBufferDX11
 {
 public:
 
+	// Constructor; create a new constant buffer of the given size
+	ConstantBufferDX11(UINT buffer_size);
+
 	// Bind this resource to the given shader target
 	void						Bind(Shader::Type shadertype, Shader::SlotID slot_id);
 
 	// Remove this (or any) binding from the given shader target
 	void						Unbind(Shader::Type shadertype, Shader::SlotID slot_id);
 
+	// Default destructor
+	~ConstantBufferDX11(void);
 
 private:
 
+	// Constant buffers should always be limited to one item.  Buffersize is the preallocated size on construction
+	ID3D11Buffer *				m_buffer[1];
+	UINT						m_buffersize;
+
+	// Static null buffer resource, used for more efficient unbinding
+	static ID3D11Buffer * const	m_null_buffer[1];
 
 };
