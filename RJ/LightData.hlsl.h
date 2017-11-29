7f9638f9e7b43a04f425fa763cdd3d00eade4336
Rob Jenks <robjenks@gmail.com>
2017-11-29 18:01:54 +0000

    Implemented LightData structure for new deferred lighting model

---
+++ RJ/LightData.hlsl.h
@@ -0,0 +1,69 @@
+#ifndef __LightDataHLSLH__
+#define __LightDataHLSLH__
+
+// Enable common usage across C++ and HLSL by making preprocessor adjustments
+#ifdef __cplusplus
+	#include <DirectXMath.h>
+
+	using float3 = DirectX::XMFLOAT3;
+	using float4 = DirectX::XMFLOAT4;
+#endif
+
+	// Supported light types
+	enum LightType
+	{
+		Point = 0, 
+		Spotlight = 1, 
+		Directional = 2
+	};
+
+	// Primary structure holding light data
+	struct LightData
+	{
+		float4					PositionWS;							// Light position (world space).  Relevant to point & spot lights
+		//------------------------------------------ ( 16 bytes )
+		float4					DirectionWS;						// Light direction (world space).  Relevant to directional & spot lights
+		//------------------------------------------ ( 16 bytes )
+		float4					PositionVS;							// Light position (view space).  Relevant to point & spot lights
+		//------------------------------------------ ( 16 bytes )
+		float4					DirectionVS;						// Light direction (view space).  Relevant to directional & spot lights
+		//------------------------------------------ ( 16 bytes )
+		float4					Colour;								// Light colour.  Incorporates diffuse + specular components; we don't currently differentiate
+		//------------------------------------------ ( 16 bytes )
+		LightType				Type;								// The type of light being rendered
+		bool					Enabled;							// Disabled light will be skipped entirely during lighting calculations.  TODO: bool portable across cpp/hlsl?
+		float					Range;								// Range at which light is fully-attenuated and no longer has any effect
+		float					Intensity;							// Overall intensity of the light
+		//------------------------------------------ ( 16 bytes )
+		float					SpotlightCosAngle;					// Cosine of the spotlight angle.  Relevant to spotlights only
+		float3					_padding;							// To maintain alignment and packing requirements
+		//------------------------------------------ ( 16 bytes )
+		//------------------------------------------ ( 16 * 7 = 112 bytes)
+
+
+		// Default constructor for use within CPP classes
+#ifdef __cplusplus
+		inline LightData(void)
+			: PositionWS(0, 0, 0, 1)
+			, DirectionWS(0, 0, -1, 0)
+			, PositionVS(0, 0, 0, 1)
+			, DirectionVS(0, 0, 1, 0)
+			, Colour(1, 1, 1, 1)
+			, Type(LightType::Point)
+			, Enabled(true)
+			, Range(100.0f)
+			, Intensity(1.0f)
+			, SpotlightCosAngle(0.7854) // == PI/4 == 45 degrees
+		{}
+#endif
+
+	};
+
+
+	// TODO: Consider reinstating attenuation parameters from previous implementation as alternative to smoothstep based on range
+	
+
+
+
+
+#endif
