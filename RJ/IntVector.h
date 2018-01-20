c056ee76f0306eb72c1f5f0e6a718a5184d1d189
Rob Jenks <robjenks@gmail.com>
2018-01-20 14:10:32 +0000

    Parameterised IntegralVector to support both int and uint

--- RJ/IntVector.h
+++ RJ/IntVector.h
@@ -5,76 +5,89 @@
 #include "DX11_Core.h"
 
 
-struct INTVECTOR2
+template <typename T>
+struct IntegralVector2
 {
-	int x; int y;
+	T x; T y;
 
-	INTVECTOR2(void) { x = 0; y = 0; }
-	INTVECTOR2(int _x, int _y) { x = _x; y = _y; }
-	INTVECTOR2(float _x, float _y) { x = (int)_x; y = (int)_y; }
-	INTVECTOR2(const XMFLOAT2 & v) { x = (int)v.x; y = (int)v.y; }
-	INTVECTOR2(int _xy) { x = _xy; y = _xy; }						// For efficiency; allows setting both components to same value
+	IntegralVector2(void) : x(0), y(0) { }
+	IntegralVector2(T _x, T _y) : x(_x), y(_y) { }
+	IntegralVector2(float _x, float _y) : x(static_cast<T>(_x)), y(static_cast<T>(_y)) { }
+	IntegralVector2(const XMFLOAT2 & v) : x(static_cast<T>(v.x)), y(static_cast<T>(v.y)) { }
+	IntegralVector2(T _xy) : x(_xy), y(_xy) { }
 
-	INTVECTOR2 Abs(void) const { return INTVECTOR2(abs(x), abs(y)); }
+	IntegralVector2 Abs(void) const { return IntegralVector2(std::abs(x), std::abs(y)); }
 	bool IsZeroVector(void) { return (x == 0 && y == 0); }
 	std::string ToString(void) const { std::ostringstream s; s << "[" << x << ", " << y << ", " << "]"; return s.str(); }
 
-	INTVECTOR2& operator +=(const INTVECTOR2& rhs) { this->x += rhs.x; this->y += rhs.y; return *this; }
-	INTVECTOR2& operator -=(const INTVECTOR2& rhs) { this->x -= rhs.x; this->y -= rhs.y; return *this; }
-	INTVECTOR2& operator *=(const int scalar) { this->x *= scalar; this->y *= scalar; return *this; }
-	INTVECTOR2& operator *=(const float scalar) { this->x = (int)(this->x * scalar); this->y = (int)(this->y * scalar); return *this; }
-	bool operator ==(const INTVECTOR2& rhs) { return (this->x == rhs.x && this->y == rhs.y); }
-	bool operator !=(const INTVECTOR2& rhs) { return (this->x != rhs.x || this->y != rhs.y); }
+	IntegralVector2& operator +=(const IntegralVector2& rhs) { this->x += rhs.x; this->y += rhs.y; return *this; }
+	IntegralVector2& operator -=(const IntegralVector2& rhs) { this->x -= rhs.x; this->y -= rhs.y; return *this; }
+	IntegralVector2& operator *=(const T scalar) { this->x *= scalar; this->y *= scalar; return *this; }
+	IntegralVector2& operator *=(const float scalar) { this->x = static_cast<T>(this->x * scalar); this->y = static_cast<T>(this->y * scalar); return *this; }
+	bool operator ==(const IntegralVector2& rhs) { return (this->x == rhs.x && this->y == rhs.y); }
+	bool operator !=(const IntegralVector2& rhs) { return (this->x != rhs.x || this->y != rhs.y); }
 };
-inline INTVECTOR2 operator +(INTVECTOR2 lhs, const INTVECTOR2& rhs) { lhs += rhs; return lhs; }
-inline INTVECTOR2 operator -(INTVECTOR2 lhs, const INTVECTOR2& rhs) { lhs -= rhs; return lhs; }
-inline INTVECTOR2 operator *(INTVECTOR2 lhs, const int scalar) { lhs *= scalar; return lhs; }
-inline INTVECTOR2 operator *(INTVECTOR2 lhs, const float scalar) { lhs *= scalar; return lhs; }
+template <typename T> inline IntegralVector2<T> operator +(IntegralVector2<T> lhs, const IntegralVector2<T> & rhs) { lhs += rhs; return lhs; }
+template <typename T> inline IntegralVector2<T> operator -(IntegralVector2<T> lhs, const IntegralVector2<T> & rhs) { lhs -= rhs; return lhs; }
+template <typename T> inline IntegralVector2<T> operator *(IntegralVector2<T> lhs, const T scalar) { lhs *= scalar; return lhs; }
+template <typename T> inline IntegralVector2<T> operator *(IntegralVector2<T> lhs, const float scalar) { lhs *= scalar; return lhs; }
+
+typedef IntegralVector2<int>			INTVECTOR2;
+typedef IntegralVector2<unsigned int>	UINTVECTOR2;
 
 
-struct INTVECTOR3
+template <typename T>
+struct IntegralVector3
 {
-	int x; int y; int z;
+	T x; T y; T z;
 
-	INTVECTOR3(void) { x = 0; y = 0; z = 0; }
-	INTVECTOR3(int _x, int _y, int _z) { x = _x; y = _y; z = _z; }
-	INTVECTOR3(float _x, float _y, float _z) { x = (int)_x; y = (int)_y; z = (int)_z; }
-	INTVECTOR3(const XMFLOAT3 & v) { x = (int)v.x; y = (int)v.y; z = (int)v.z; }
-	INTVECTOR3(int val) { x = y = z = val; }
+	IntegralVector3(void) : x(0), y(0), z(0) { }
+	IntegralVector3(T _x, T _y, T _z) : x(_x), y(_y), z(_z) { }
+	IntegralVector3(float _x, float _y, float _z) : x(static_cast<T>(_x)), y(static_cast<T>(_y)), z(static_cast<T>(_z)) { }
+	IntegralVector3(const XMFLOAT3 & v) : x(static_cast<T>(v.x)), y(static_cast<T>(v.y)), z(static_cast<T>(v.z)) { }
+	IntegralVector3(T xyz) : x(xyz), y(xyz), z(xyz) { }
 
-	INTVECTOR3 Abs(void) const { return INTVECTOR3(abs(x), abs(y), abs(z)); }
+	IntegralVector3 Abs(void) const { return IntegralVector3(std::abs(x), std::abs(y), std::abs(z)); }
 	bool IsZeroVector(void) { return (x == 0 && y == 0 && z == 0); }
 
-	INTVECTOR3& operator +=(const INTVECTOR3& rhs) { this->x += rhs.x; this->y += rhs.y; this->z += rhs.z; return *this; }
-	INTVECTOR3& operator -=(const INTVECTOR3& rhs) { this->x -= rhs.x; this->y -= rhs.y; this->z -= rhs.z; return *this; }
-	INTVECTOR3& operator *=(const int scalar) { this->x *= scalar; this->y *= scalar; this->z *= scalar; return *this; }
-	INTVECTOR3& operator *=(const float scalar) {
-		this->x = (int)(this->x * scalar); this->y = (int)(this->y * scalar);
-		this->z = (int)(this->z * scalar); return *this;
+	IntegralVector3 & operator +=(const IntegralVector3 & rhs) { this->x += rhs.x; this->y += rhs.y; this->z += rhs.z; return *this; }
+	IntegralVector3 & operator -=(const IntegralVector3 & rhs) { this->x -= rhs.x; this->y -= rhs.y; this->z -= rhs.z; return *this; }
+	IntegralVector3 & operator *=(const int scalar) { this->x *= scalar; this->y *= scalar; this->z *= scalar; return *this; }
+	IntegralVector3 & operator *=(const float scalar) {
+		this->x = static_cast<T>(this->x * scalar); this->y = static_cast<T>(this->y * scalar);
+		this->z = static_cast<T>(this->z * scalar); return *this;
 	}
-	INTVECTOR3& operator /=(const int scalar) { this->x /= scalar; this->y /= scalar; this->z /= scalar; return *this; }
-	INTVECTOR3& operator /=(const float scalar) {
-		this->x = (int)((float)this->x / scalar); this->y = (int)((float)this->y / scalar);
-		this->z = (int)((float)this->z / scalar); return *this;
+	IntegralVector3& operator /=(const T scalar) { this->x /= scalar; this->y /= scalar; this->z /= scalar; return *this; }
+	IntegralVector3& operator /=(const float scalar) {
+		this->x = static_cast<T>((float)this->x / scalar); this->y = static_cast<T>((float)this->y / scalar);
+		this->z = static_cast<T>((float)this->z / scalar); return *this;
 	}
-	INTVECTOR3& operator +=(const int scalar) { this->x += scalar; this->y += scalar; this->z += scalar; return *this; }
-	INTVECTOR3& operator -=(const int scalar) { this->x -= scalar; this->y -= scalar; this->z -= scalar; return *this; }
-	bool operator ==(const INTVECTOR3& rhs) { return (this->x == rhs.x && this->y == rhs.y && this->z == rhs.z); }
-	bool operator !=(const INTVECTOR3& rhs) { return (this->x != rhs.x || this->y != rhs.y || this->z != rhs.z); }
-	bool operator ==(const INTVECTOR3& rhs) const { return (this->x == rhs.x && this->y == rhs.y && this->z == rhs.z); }
-	bool operator !=(const INTVECTOR3& rhs) const { return (this->x != rhs.x || this->y != rhs.y || this->z != rhs.z); }
+	IntegralVector3& operator +=(const T scalar) { this->x += scalar; this->y += scalar; this->z += scalar; return *this; }
+	IntegralVector3& operator -=(const T scalar) { this->x -= scalar; this->y -= scalar; this->z -= scalar; return *this; }
+	bool operator ==(const IntegralVector3 & rhs) { return (this->x == rhs.x && this->y == rhs.y && this->z == rhs.z); }
+	bool operator !=(const IntegralVector3 & rhs) { return (this->x != rhs.x || this->y != rhs.y || this->z != rhs.z); }
+	bool operator ==(const IntegralVector3 & rhs) const { return (this->x == rhs.x && this->y == rhs.y && this->z == rhs.z); }
+	bool operator !=(const IntegralVector3 & rhs) const { return (this->x != rhs.x || this->y != rhs.y || this->z != rhs.z); }
 
-	bool operator<(const INTVECTOR3 &rhs) const { return (x < rhs.x && y < rhs.y && z < rhs.z); }
-	bool operator<=(const INTVECTOR3 &rhs) const { return (x <= rhs.x && y <= rhs.y && z <= rhs.z); }
-	bool operator>(const INTVECTOR3 &rhs) const { return (x > rhs.x && y > rhs.y && z > rhs.z); }
-	bool operator>=(const INTVECTOR3 &rhs) const { return (x >= rhs.x && y >= rhs.y && z >= rhs.z); }
+	bool operator<(const IntegralVector3 & rhs) const { return (x < rhs.x && y < rhs.y && z < rhs.z); }
+	bool operator<=(const IntegralVector3 & rhs) const { return (x <= rhs.x && y <= rhs.y && z <= rhs.z); }
+	bool operator>(const IntegralVector3 & rhs) const { return (x > rhs.x && y > rhs.y && z > rhs.z); }
+	bool operator>=(const IntegralVector3 & rhs) const { return (x >= rhs.x && y >= rhs.y && z >= rhs.z); }
 
 	XMFLOAT3 ToFloat3(void) const { return XMFLOAT3((float)x, (float)y, (float)z); }
 	std::string ToString(void) const { std::ostringstream s; s << "[" << x << ", " << y << ", " << z << "]"; return s.str(); }
 };
-inline INTVECTOR3 operator +(INTVECTOR3 lhs, const INTVECTOR3& rhs) { lhs += rhs; return lhs; }
-inline INTVECTOR3 operator -(INTVECTOR3 lhs, const INTVECTOR3& rhs) { lhs -= rhs; return lhs; }
-inline INTVECTOR3 operator *(INTVECTOR3 lhs, const int scalar) { lhs *= scalar; return lhs; }
-inline INTVECTOR3 operator *(INTVECTOR3 lhs, const float scalar) { lhs *= scalar; return lhs; }
-inline INTVECTOR3 operator /(INTVECTOR3 lhs, const int scalar) { lhs /= scalar; return lhs; }
-inline INTVECTOR3 operator /(INTVECTOR3 lhs, const float scalar) { lhs /= scalar; return lhs; }
+template <typename T> inline IntegralVector3<T> operator +(IntegralVector3<T> lhs, const IntegralVector3<T>& rhs) { lhs += rhs; return lhs; }
+template <typename T> inline IntegralVector3<T> operator -(IntegralVector3<T> lhs, const IntegralVector3<T>& rhs) { lhs -= rhs; return lhs; }
+template <typename T> inline IntegralVector3<T> operator *(IntegralVector3<T> lhs, const int scalar) { lhs *= scalar; return lhs; }
+template <typename T> inline IntegralVector3<T> operator *(IntegralVector3<T> lhs, const float scalar) { lhs *= scalar; return lhs; }
+template <typename T> inline IntegralVector3<T> operator /(IntegralVector3<T> lhs, const int scalar) { lhs /= scalar; return lhs; }
+template <typename T> inline IntegralVector3<T> operator /(IntegralVector3<T> lhs, const float scalar) { lhs /= scalar; return lhs; }
+
+typedef IntegralVector3<int>			INTVECTOR3;
+typedef IntegralVector3<UINT>			UINTVECTOR3;
+
+
+
+
+
