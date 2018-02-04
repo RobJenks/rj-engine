46bbd55ea860073e95dfa30e6a3730e5c62953ae
Rob Jenks <robjenks@gmail.com>
2018-02-04 22:25:17 +0000

    Added bulk RJM-to-OBJ conversion capability

--- ModelPipeline/ModelPipeline.cpp
+++ ModelPipeline/ModelPipeline.cpp
@@ -48,6 +48,43 @@ void RjmObjConversionTest(void)
 	// auto m = ModelData::Deserialize(PipelineUtil::ReadBinaryFile(fs::path("C:\\Users\\robje\\Downloads\\capsule.out")));
 }
 
+void BulkRjmObjConversion(void)
+{
+	// Basic pipeline configuration
+	std::unique_ptr<TransformPipeline> pipeline = TransformPipelineBuilder()
+		.WithInputTransformer(std::move(std::make_unique<InputTransformerRjm>("", true)))
+		.WithPipelineStage(std::move(std::make_unique<PipelineStagePassthrough>()))
+		.WithPipelineStage(std::move(std::make_unique<PipelineStageCentreModel>()))
+		.WithPipelineStage(std::move(std::make_unique<PipelineStagePassthrough>()))
+		.WithOutputTransformer(std::move(std::make_unique<BinaryOutputTransform>()))
+		.Build();
+
+	// Set of models to be converted
+	std::vector<fs::path> source_files = 
+	{
+		fs::path("C:\\Users\\robje\\Downloads\\testship1.rjm")
+	};
+
+	int index = 0;
+	for (auto path : source_files)
+	{
+		std::cout << "> Processing file " << ++index << " of " << source_files.size() << " (" << path.filename().string() << ")\n";
+
+		// Execute the transformation pipeline for each model in turn
+		fs::path target(fs::absolute(path.parent_path()).string() + "/" + path.stem().string() + ".out");
+		pipeline->Transform(path, target);
+
+		// Rename the original file in order to retain a backup (do not overwrite so that multiple runs will not eliminate backups)
+		fs::copy_file(path, fs::path(path.string() + ".backup"), fs::copy_options::skip_existing);
+
+		// Rename the target file to the original file to perform an in-place swap
+		fs::copy_file(target, path, fs::copy_options::overwrite_existing);
+
+		// Remove the target file now that it has been swapped in for the original file
+		fs::remove(target);
+	}
+
+}
 
 
 
@@ -55,7 +92,8 @@ void RjmObjConversionTest(void)
 int main(void)
 {
 	//ObjTransformTest();
-	RjmObjConversionTest();
+	//RjmObjConversionTest();
+	BulkRjmObjConversion();
 
 	// Return success
 	std::cout << "\n";
