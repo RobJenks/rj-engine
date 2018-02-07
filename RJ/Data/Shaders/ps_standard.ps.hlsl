3f26c7cfb257cf0552e25a26e13d34fb9c715363
Rob Jenks <robjenks@gmail.com>
2018-02-07 22:24:19 +0000

    Minor update to PS to force-introduce sampler to compiled shader

--- RJ/Data/Shaders/ps_standard.ps.hlsl
+++ RJ/Data/Shaders/ps_standard.ps.hlsl
@@ -1,10 +1,8 @@
 #include "../../CommonShaderPipelineStructures.hlsl.h"
 #include "../../CommonShaderBufferDefinitions.hlsl.h"
 #include "../../../Definitions/VertexDefinitions.hlsl.h"
-
-
 // PS_Standard: Standard pixel shader
 float4 PS_Standard(VertexShaderStandardOutput IN) : SV_TARGET
 {
-	return float4(0,0,0,0);
+	return (DiffuseTexture.Sample(LinearRepeatSampler, IN.texCoord));
 }
