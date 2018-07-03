183e438ea1438bc0d8ca4a99921fe52bfa6a6aae
Rob Jenks <robjenks@gmail.com>
2018-07-03 21:24:02 +0100

    Fixed runtime device failure  during debug rendering with incorrect VS input type

--- RJ/Data/Shaders/deferred_ps_debug.ps.hlsl
+++ RJ/Data/Shaders/deferred_ps_debug.ps.hlsl
@@ -10,7 +10,7 @@ Texture2D DebugSourceTextureVS : register(t0);
 
 // Pixel shader that generates the G-Buffer
 [earlydepthstencil]
-float4 PS_Deferred_Debug(VertexShaderStandardOutput IN) : SV_Target0
+float4 PS_Deferred_Debug(ScreenSpaceQuadVertexShaderOutput IN) : SV_Target0
 {
 	static const float DEPTH_EXP_FACTOR = 6.0f;
 
