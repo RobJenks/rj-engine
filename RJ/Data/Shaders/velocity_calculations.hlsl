7e3c4bb90b9bba5c00639bf14709bf2a01588de6
Rob Jenks <robjenks@gmail.com>
2018-07-21 23:11:27 +0100

    Remove framerate dependency for screen-space velocity calculations, as at least a temporary test

--- RJ/Data/Shaders/velocity_calculations.hlsl
+++ RJ/Data/Shaders/velocity_calculations.hlsl
@@ -4,13 +4,23 @@
 #include "hlsl_common.hlsl"
 #include "DeferredRenderingBuffers.hlsl"
 
+// Removes framerate dependency for velocity calculation if set.  Seems to give more reliable results in TAA
+#define REMOVE_VELOCITY_FRAMERATE_DEPENDENCE		1
+
+
+static const float FRAME_RATE_RECIP_60FPS = (1.0f / 60.0f);
 
 // Calculate the normalised screen-space velocity of the given pixel, based on supplied data
 // for the current and prior frame.  Velocity data is normalised down within the 
 // range [0 1] for screen-space velocity [0 MAX_SCREEN_SPACE_PIXEL_VELOCITY]
 float2 CalculateScreenSpacePixelVelocity(float4 prior_pos, float4 current_pos)
 {
-	float2 vraw = ((current_pos.xy / current_pos.w) - (prior_pos.xy / prior_pos.w)) * C_half_frame_exposure;
+	float2 vraw = ((current_pos.xy / current_pos.w) - (prior_pos.xy / prior_pos.w))
+#if REMOVE_VELOCITY_FRAMERATE_DEPENDENCE
+		* ((2.0f * C_half_exposure) / FRAME_RATE_RECIP_60FPS) * 0.5f;	// Equiv of C_half_frame_exposure calc but with 1/60 instead of 1/TimeFactor
+#else
+		* C_half_frame_exposure;
+#endif
 
 	float vmagnitude = length(vraw);
 
